25d303ab759030e0108a19f325a9e625e9e88030
Artem Mikhailob <mobail_2003@bk.ru>
2022-07-08 15:34:24 +0300

    homework_9 ver.1 task_60

---
+++ task_64/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 64: Задайте значения M и N. Напишите программу, которая выведет произведение натуральных чисел в промежутке от M до N.
+
+M = 1; N = 5. ->120
+
+M = 4; N = 6. -> 120
+*/
+using static System.Console;
+
+Clear();
+Write("Введите значение м:");
+int m=int.Parse(ReadLine());
+Write("Введите значение N:");
+int n=int.Parse(ReadLine());
+WriteLine(FourierMultiplication(m,n));
+
+
+int FourierMultiplication(int a, int b)
+{
+    int result=b;
+    while(b>a)
+    {
+        b--;
+        result*=b;
+    }
+    return result;
+}
