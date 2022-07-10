a051f413a4b8e6998ac34c3733a367cc0714338a
Mikhaylov Artem <gits.info20@gmail.com>
2022-07-10 22:47:56 +0300

    homework_9 ver.2 task_66

--- task_66/Program.cs
+++ task_66/Program.cs
@@ -9,15 +9,34 @@ M = 4; N = 8. -> 30
 using static System.Console;
 
 Clear();
-Write("Введите значение м:");
+Write("Введите значение M:");
 int m=int.Parse(ReadLine());
 Write("Введите значение N:");
 int n=int.Parse(ReadLine());
-WriteLine(FourierMultiplication(m,n));
-
-
-
-int SumNumbers (int m, int n)
+if(n>=m)
 {
-    int count=n>=m? count=n-m
+    int count=n-m;
+    int start=m;
+    int end=n;
+    WriteLine($"Сумма чисел от {start} до {end} равна: {FourierMultiplication(start, end, count)}");
+}
+else
+{
+    int count=m-n;
+    int start=n;
+    int end = m;
+    WriteLine($"Сумма чисел от {start} до {end} равна: {FourierMultiplication(start, end, count)}");
+}
+
+int FourierMultiplication(int a, int b, int c)
+{
+    int temp;
+    if(c>0)
+    {
+        b=b+a;
+        a++;
+        c--;
+        return FourierMultiplication(a, b, c);
+    }
+    else return b;
 }
