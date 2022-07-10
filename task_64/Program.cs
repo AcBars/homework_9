a8e9a261b172b478a49dc4841462c57f9d797405
Mikhaylov Artem <gits.info20@gmail.com>
2022-07-10 21:18:57 +0300

    homework_9 ver.3 task_64

--- task_64/Program.cs
+++ task_64/Program.cs
@@ -1,4 +1,5 @@
-﻿/*Задача 64: Задайте значения M и N. Напишите программу, которая выведет произведение натуральных чисел в промежутке от M до N.
+﻿/*Задача 64: Задайте значения M и N. Напишите программу, которая выведет произведение
+натуральных чисел в промежутке от M до N.
 
 M = 1; N = 5. ->120
 
@@ -9,32 +10,51 @@ using static System.Console;
 Clear();
 Write("Введите значение м:");
 int m=int.Parse(ReadLine());
-Write("Введите значение N:");
+Write("Введите значение n:");
 int n=int.Parse(ReadLine());
-WriteLine(FourierMultiplication(m,n));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int FourierMultiplication(int a, int b)
+if(n>m)
 {
-    int result=b;
-    if(b>=a)
+    int count=n-m;
+    int start=m;
+    int end=n;
+    FourierMultiplication(start, end, count);
+}
+else
+{
+    int count=m-n;
+    int start=n;
+    int end = m;
+    FourierMultiplication(start, end, count);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void FourierMultiplication(int a, int b, int c)
+{
+    
+    if(c>=0)
     {
-        b--;
-        result*=FourierMultiplication(result, b);
         
-        return result;
+        
+        WriteLine($"{a}*{b}={a*b}");
+        a=b*a;
+        b--;
+        
+        c--;
+        FourierMultiplication(a, b, c);
+        
+        
     }
-    else return result; 
+    else return; 
 }
